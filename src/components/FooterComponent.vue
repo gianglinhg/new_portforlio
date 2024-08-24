6a9f653f01ddfdaa6f5c90486e3b0da124f2e591
Giang Linh <lucas@digityze.asia>
2024-08-24 13:20:18 +0700

    [add]-footer_font_awesome

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,5 +1,29 @@
 <template>
   <div>
-    Trang FOOTER
+    <div class="text-sm font-light bg-white flex items-center justify-between h-16 px-5">
+      <div class="basis-2/6">
+        <p class="">&copy; 2035 by Maya Nelson.</p>
+        <p class="">Powered and secured by Wix</p>
+      </div>
+      <div class="flex basis-4/6 gap-x-10 justify-center">
+        <div class="leading-7">
+          <h5 class="text-center font-medium">Call</h5>
+          <p>123-456-7890</p>
+        </div>
+        <div class="leading-7">
+          <h5 class="text-center font-medium">Write</h5>
+          <p>test@email.com</p>
+        </div>
+        <div class="leading-7">
+          <h5 class="text-center font-medium">Follow</h5>
+          <ul class="flex gap-x-2">
+            <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
+            <li><a href="#"><i class="fa-brands fa-linkedin"></i></a></li>
+            <li><a href="#"><i class="fa-brands fa-linkedin"></i></a></li>
+            <li><a href="#"><i class="fa-brands fa-linkedin"></i></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -9,15 +9,17 @@
         <span class="hidden md:inline-block">/</span>
         <h5 class="text-xl font-light">PHP Developer</h5>
       </div>
-      <ul
-        class="w-full h-screen fixed top-0 left-0 justify-center items-center flex flex-col gap-3 bg-yellow-ce md:h-auto md:w-auto md:bg-opacity-0 md:flex-row md:static md:justify-end md:visible"
-        :class="{ 'invisible': !isShowMenu }">
-        <li><a href="#" class="c-link">About me</a></li>
-        <li><a href="#" class="c-link">Resume</a></li>
-        <li><a href="#" class="c-link">Projects</a></li>
-        <li><a href="#" class="c-link">Contact</a></li>
-      </ul>
-      <div class="block md:hidden w-8 h-8 z-20" @click="isShowMenu = !isShowMenu">
+      <Transition name="slide-fade" mode="out-in">
+        <ul
+          class="w-full h-screen fixed top-0 left-0 justify-center items-center flex flex-col gap-3 bg-yellow-ce md:h-auto md:w-auto md:bg-opacity-0 md:flex-row md:static md:justify-end md:visible"
+          :class="{ 'invisible': !isShowMenu }" v-if="isShowMenu">
+          <li><a href="#" class="c-link">About me</a></li>
+          <li><a href="#" class="c-link">Resume</a></li>
+          <li><a href="#" class="c-link">Projects</a></li>
+          <li><a href="#" class="c-link">Contact</a></li>
+        </ul>
+      </Transition>
+      <div class="cursor-pointer block md:hidden w-8 h-8 z-20" @click="isShowMenu = !isShowMenu">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
           class="w-full h-full" :class="[isShowMenu ? 'text-black' : 'text-blue-ff']">
           <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
@@ -26,7 +28,35 @@
     </div>
   </div>
 </template>
+
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted, onUnmounted } from 'vue';
 const isShowMenu = ref(false);
+
+onMounted(() => {
+  window.addEventListener("resize", handleWindowSizeChange);
+  handleWindowSizeChange();
+});
+onUnmounted(() => {
+  window.removeEventListener("resize", handleWindowSizeChange);
+});
+const handleWindowSizeChange = () => {
+  isShowMenu.value = true;
+};
 </script>
+
+<style scoped>
+.slide-fade-enter-active {
+  transition: all 0.3s ease-in;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateY(20px);
+  opacity: 0;
+}
+</style>
